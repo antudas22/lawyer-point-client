9a8e0f2fc0a6951f9ae89bb5dd8140683d4d790b
antudas22 <20antu20@gmail.com>
2023-04-28 21:26:41 +0600

    created topnav and navbar

--- src/Pages/Shared/TopNav/TopNav.js
+++ src/Pages/Shared/TopNav/TopNav.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { HiPhone, HiOutlineMail } from "react-icons/hi";
 
 const TopNav = () => {
     return (
-        <div>
-            TopNav
+        <div className='flex gap-5 justify-end py-1 pr-8 bg-gray-300'>
+            <div className='flex items-center gap-2'>
+            <HiPhone/><p>+123 4567 83121</p>
+            </div>
+            <p>|</p>
+            <div className='flex items-center gap-2'>
+            <HiOutlineMail/><p>lawyerpoint@gmail.com</p>
+            </div>
         </div>
     );
 };
